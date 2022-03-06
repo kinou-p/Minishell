5fd823ca04a5eccc5b3b205b7539c84b49cf19f7
kinou-p <alexpomms@gmail.com>
2022-03-06 13:54:28 +0100

    first

---
+++ Makefile
@@ -0,0 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: apommier <apommier@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/03/06 12:50:24 by apommier          #+#    #+#              #
+#    Updated: 2022/03/06 13:53:49 by apommier         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+NAME	= minishell
+SRCS	= main.c
+OBJS	= ${SRCS:.c=.o}
+CC		= gcc
+CFLAGS	= -Wall -Wextra
+#CFLAGS	= -Wall -Wextra -Werror
+RM		= rm -rf
+LIBFT	= ./libft
+
+${NAME}: ${OBJS} 
+		@make bonus -C ${LIBFT} 
+		@${CC} ${CFLAGS} ${OBJS} ${LIBFT}/libft.a -o ${NAME}
+	
+all:	${NAME} bonus
+
+clean:
+		@${RM} ${OBJS}
+		@make clean -C ${LIBFT}
+
+fclean:	clean
+		@${RM} ${NAME}
+		@make fclean -C ${LIBFT}
+		
+re: 	fclean all
+
+.PHONY: all clean fclean re bonus
---
+++ libft/ft_strncmp.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apommier <apommier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/29 00:13:31 by apommier          #+#    #+#             */
+/*   Updated: 2022/02/14 00:27:01 by apommier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		n--;
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (1);
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
