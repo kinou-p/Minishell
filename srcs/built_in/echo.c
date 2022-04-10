467e46a948089edb518ba304aeba91cb9d943dd3
PrStein <eliseesydney@gmail.com>
2022-04-10 22:21:27 +0200

    PWD

--- srcs/built_in/echo.c
+++ srcs/built_in/echo.c
@@ -12,32 +12,32 @@
 
 #include "../../includes/minishell.h"
 
-int	jump_space(char *str, int i)
-{
-	int	j;
+// int	jump_space(char *str, int i)
+// {
+// 	int	j;
 
-	j = i;
-	while (str[j] == ' ')
-		j++;
-	return (j);
-}
+// 	j = i;
+// 	while (str[j] == ' ')
+// 		j++;
+// 	return (j);
+// }
 
-void	print_value(char *str, char *input)
-{
-	(void)input;
-	int i;
+// void	print_value(char *str, char *input)
+// {
+// 	(void)input;
+// 	int i;
 
-	i = 0;
-	while (str[i] && str[i] != '=')
-		i++;
-	i++;
-	ft_putstr_fd(str + i, 0);
-	/*while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}*/
-}
+// 	i = 0;
+// 	while (str[i] && str[i] != '=')
+// 		i++;
+// 	i++;
+// 	ft_putstr_fd(str + i, 0);
+// 	/*while (str[i])
+// 	{
+// 		ft_putchar(str[i]);
+// 		i++;
+// 	}*/
+// }
 
 // void	print_rest(char *input, char *str, int j)
 // {
@@ -70,66 +70,66 @@ void	print_value(char *str, char *input)
 // 		return ;
 // }
 
-void	find_for_print(t_s_cmd *cmd, char *input)
-{
-	char *str;
-	// char *tmp;
-	int	i;
-	int x;
+// void	find_for_print(t_s_cmd *cmd, char *input)
+// {
+// 	char *str;
+// 	// char *tmp;
+// 	int	i;
+// 	int x;
 
-	i = 0;
-	// j = j + 1;
-	// tmp = ft_substr(input, j, find_len(input, j, 32));
-	// printf("%c\n", input[j]);
-	str = ft_strjoin(input, "=");
-	x = ft_strlen(str);
-	// printf("%d\n", x);
-	while (cmd->big_cmd->env[i])
-	{
-		if (ft_strncmp(cmd->big_cmd->env[i], str, x) == 0)
-		{
-			// printf("%s\n", cmd->big_cmd->env[i]);
-			break ;
-		}
+// 	i = 0;
+// 	// j = j + 1;
+// 	// tmp = ft_substr(input, j, find_len(input, j, 32));
+// 	// printf("%c\n", input[j]);
+// 	str = ft_strjoin(input, "=");
+// 	x = ft_strlen(str);
+// 	// printf("%d\n", x);
+// 	while (cmd->big_cmd->env[i])
+// 	{
+// 		if (ft_strncmp(cmd->big_cmd->env[i], str, x) == 0)
+// 		{
+// 			// printf("%s\n", cmd->big_cmd->env[i]);
+// 			break ;
+// 		}
 
-		i++;
-	}
-	if (cmd->big_cmd->env[i] != NULL)
-	{
-		print_value(cmd->big_cmd->env[i], input);
-		// print_rest(input, tmp, j);
-	}
-}
+// 		i++;
+// 	}
+// 	if (cmd->big_cmd->env[i] != NULL)
+// 	{
+// 		print_value(cmd->big_cmd->env[i], input);
+// 		// print_rest(input, tmp, j);
+// 	}
+// }
 
-int	dollar_or_not(char *str)
-{
-	int i;
+// int	dollar_or_not(char *str)
+// {
+// 	int i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '$')
-			return (1);
-		i++;
-	}
-	return (0);
-}
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '$')
+// 			return (1);
+// 		i++;
+// 	}
+// 	return (0);
+// }
 
-void	find_value(t_s_cmd *cmd, char *str)
-{
-	// int i;
-	//
-	// i = 0;
-	while (*str)
-	{
-		if (*str == '$')
-			break ;
-		printf("%c", *str);
-		str++;
-	}
-	str++;
-	find_for_print(cmd, str);
-}
+// void	find_value(t_s_cmd *cmd, char *str)
+// {
+// 	// int i;
+// 	//
+// 	// i = 0;
+// 	while (*str)
+// 	{
+// 		if (*str == '$')
+// 			break ;
+// 		printf("%c", *str);
+// 		str++;
+// 	}
+// 	str++;
+// 	find_for_print(cmd, str);
+// }
 
 void	ft_echo(t_s_cmd *d)
 {
@@ -156,10 +156,10 @@ void	ft_echo(t_s_cmd *d)
 	if (d->args[i])
 		while (d->args[i])
 		{
-			if (dollar_or_not(d->args[i]) == 1)
-				find_value(d, d->args[i]);
-			else
-				printf("%s", d->args[i]);
+			// if (dollar_or_not(d->args[i]) == 1)
+			// 	find_value(d, d->args[i]);
+			// else
+			printf("%s", d->args[i]);
 			if (d->args[i + 1] != NULL)
 				printf(" ");
 			i++;
