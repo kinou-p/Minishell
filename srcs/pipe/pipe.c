d82442382248997c9650c1da33e8f531ba53454d
kinou-p <alexpomms@gmail.com>
2022-04-15 01:10:50 +0200

    fix var child child =0 at start

--- srcs/pipe/pipe.c
+++ srcs/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: apommier <apommier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 18:51:31 by apommier          #+#    #+#             */
-/*   Updated: 2022/04/15 00:14:04 by apommier         ###   ########.fr       */
+/*   Updated: 2022/04/15 01:10:14 by apommier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,6 +124,7 @@ void	execute(t_cmd *cmd, char **env)
 		fdin=dup(tmpin);
 	while(cmd->current_s_cmd)
 	{
+		cmd->current_s_cmd->child = 0;
 		fdout = 0;
 		if (i > 0 && cmd->current_s_cmd->infile)
 		{
